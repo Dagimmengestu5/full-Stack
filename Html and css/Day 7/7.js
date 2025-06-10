4113e84f6d8dfde95bd89f33406203bc63fed927
dagimmengestu5 <dagimmengestu5@gmail.com>
2025-06-10 20:31:32 +0300

    ok

---
+++ Html and css/Day 7/7.js
@@ -0,0 +1,12 @@
+let firstname = "dagim"
+let lastname = 'mengistu'
+let age = 21
+let car;
+let animal;
+
+car = "tesla"
+animal = "dog"
+
+console.log(firstname, lastname)
+console.log("abebe")
+console.log(age,car,animal)
