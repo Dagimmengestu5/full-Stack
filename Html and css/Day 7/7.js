615dc5b51e42293023d722202a2cab7e6d2becd5
dagimmengestu5 <dagimmengestu5@gmail.com>
2025-06-28 20:51:47 +0300

    1

--- Html and css/Day 7/7.js
+++ Html and css/Day 7/7.js
@@ -1,25 +1,132 @@
-let firstname = "dagim"
-let lastname = 'mengistu'
-let age = 21
-let car;
-let animal;
+// // Banking system 
 
-car = "tesla"
-animal = "dog"
+// // withdrow
+// //deposit
+// //transfer
+// //checks the user pin
 
-console.log(firstname, lastname)
-console.log("abebe")
-console.log(age,car,animal)
-console.log(`dagi is the balebet of ${car} then he have the ${animal}`)
 
-const path = require("node:path")
-console.log(__dirname)
 
-let sum = 0;
-for (let i = 0; i <= 100; i++ ) {
-    sum = sum + i
+
+// const users = [{
+//     name: "dagim mengestu",
+//     pin: "1234",
+//     accountNumber: "1000001",
+//     balance: 1500,
+//     transactions:[]
+// },
+// {
+//     name: "betre mengestu",
+//     pin: "0001",
+//     accountNumber: "1000002",
+//     balance: 1000,
+//     transactions:[]
+// },
+// {
+//     name: "dejen sentayehu",
+//     pin: "0002",
+//     accountNumber: "1000002",
+//     balance: 2500,
+//     transactions:[]
+// },
+        
+// ]
+
+// function runBankProgram(){
+//     console.log("Welcome to our bank")
+
+//     //authentication
+//     const authenticateUser = authenticateUser()
+//     console.log(authenticateUser)
+//     if (authenticateUser){
+//         console.log(`Welcome, ${authenticateUser.user}`)
+//     }else{
+//         console.log("authentication faild,")
+//     }
+// }
+
+
+
+// function authenticateUser (){
+//     let attempts = 3;
+
+//     while (attempts > 0 ){
+//         //prompt is an inbuit function used to get input
+//         const enterdPin = prompt("Enter your pin(or type 'exit to quit)")
+//         if (enterdPin?.toLocaleLowerCase() === "exit"){
+//             return null
+//         }
+//         //we need to find the user 
+//         const user = users.find ((bankUser)=> {
+//             return bankUser.pin === enterdPin 
+//         })
+//         //null, undifined , 0, false ==== false
+//         if (user){
+//             return user
+//         }else{
+//             attempts = attempts -1;
+//             console.log(`Invalid pin ${attempts} attempts are remaining `)
+//         }
+//     }
+//     return null
+// }
+// runBankProgram()
+
+const users = [
+    {
+        name: "dagim mengestu",
+        pin: "1234",
+        accountNumber: "1000001",
+        balance: 1500,
+        transactions:[]
+    },
+    {
+        name: "betre mengestu",
+        pin: "0001",
+        accountNumber: "1000002",
+        balance: 1000,
+        transactions:[]
+    },
+    {
+        name: "dejen sentayehu",
+        pin: "0002",
+        accountNumber: "1000003",
+        balance: 2500,
+        transactions:[]
+    },
+];
+
+function runBankProgram() {
+    console.log("Welcome to our bank");
+
+    const user = authenticateUser();
+    if (user) {
+        console.log(`Welcome, ${user.name}`);
+    } else {
+        console.log("Authentication failed.");
+    }
 }
-console.log(`the sum of the furst 100 natural numcer is ${sum}`)
 
-const frutes = ["banana","mango","orenge"]
-console.log(frutes.length)
+function authenticateUser() {
+    let attempts = 3;
+
+    while (attempts > 0) {
+        const enteredPin = prompt("Enter your pin (or type 'exit' to quit)");
+
+        if (enteredPin?.toLowerCase() === "exit") {
+            return null;
+        }
+
+        const user = users.find(bankUser => bankUser.pin === enteredPin);
+        if (user) {
+            return user;
+        } else {
+            attempts--;
+            console.log(`Invalid pin. ${attempts} attempt(s) remaining.`);
+        }
+    }
+
+    return null;
+}
+
+runBankProgram();
