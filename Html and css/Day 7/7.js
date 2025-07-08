a5734f3db842742b09b303268046854ab655e4b9
dagimmengestu5 <dagimmengestu5@gmail.com>
2025-07-08 20:31:43 +0300

    xc

--- Html and css/Day 7/7.js
+++ Html and css/Day 7/7.js
@@ -353,7 +353,7 @@
 
 // // console.log(person.greetPeople("abebe"))
 
-// sd c
+// sd cxc
 
 const newDiv = document.createElement("div")
 const parent = document.querySelector(".parent-container")
