6eafeef7728d27600303b400c7ace1cd5d2aa213
dagimmengestu5 <dagimmengestu5@gmail.com>
2025-07-09 20:48:14 +0300

    1

--- Html and css/Day 7/7.js
+++ Html and css/Day 7/7.js
@@ -1,377 +1,176 @@
-// // 1.bank -system
-// // -withdraw money
-// // -deposit money
-// // -transfer money
-// // check balances
+// by using its id getElementById()
 
-// // checks for the user pin
 
+const header = document.getElementById('header')
+const radiobuttons = document.getElementsByName("gender")
+// console.log(header)
+// console.log(radiobuttons)
+const paragraph = document.getElementsByTagName('p')
+// console.log(paragraph)
 
-// const users = [
-// {
-//     name:"James Williams",
-//     pin:"1234",
-//     accountNumber:"10000001",
-//     balance:1500,
-//     transcations:[]
-// },
-// {
-//     name:"Sarah Adams",
-//     pin:"0001",
-//     accountNumber:"10000002",
-//     balance:3500,
-//     transcations:[]
-// },
-// {
-//     name:"Michael Genanew",
-//     pin:"1021",
-//     accountNumber:"10000003",
-//     balance:5000,
-//     transcations:[]
-// }
-// ]
+const specialParagraph = document.getElementsByClassName("special")
+// console.log(specialParagraph)
 
+const newSpecialParagraph = document.querySelector(".special")
+const allspecialParagraph = document.querySelectorAll(".special")
 
-// // console.log(users[1].name,users[1].pin)
+// console.log(newSpecialParagraph)
+// console.log(allspecialParagraph)
 
 
+// const child = document.querySelector(".child")
 
-// function runBankProgram(){
-//     console.log("Welcome to our Bank")
+// const parent = child.parentElement;
 
-//     // authentication function
-//     const authenticatedUser = authenticateUser()
-//     // console.log(authenticatedUser)
+// console.log(child,parent)
 
-//     if(authenticatedUser){
-//         console.log(`Welcome,${authenticatedUser.name}`)
-//         // showMenu(authenticateUser)
-//         showMenu(authenticatedUser);
-//     }else{
-//         console.log("authentication failed, exiting the application")
-//     }
+// const parent = document.querySelector(".parent-container")
+// const allChildren = parent.children;
+// console.log(parent,allChildren)
 
-// }
-
-// runBankProgram()
-
-
-// function authenticateUser(){
-//     let attempts = 3;
-
-//     while(attempts > 0){
-//         // prompt is an inbuilt function used to get input from our users
-//         const enteredPin = prompt("Enter your Pin(or type 'exit' to quit)")
-    
-//         if(enteredPin?.toLocaleLowerCase()==="exit"){
-//             return null
-//         }
-
-//         // we need to find the user
-
-//         const user = users.find((bankUser)=>{
-//          return   bankUser.pin === enteredPin 
-//         })
-// // null,undefined,0, flase ==== false
-//         if(user){
-//             return user
-//         }else{
-//             attempts = attempts -1;
-//             console.log(`Invalid pin ${attempts} attempts remaing`)
-//         }
-
-
-//     }
-
-//     return null
-// }
-
-
-// function showMenu(user){
-//     let continueBanking = true;
-//     while(continueBanking){
-//     console.log("Please select an option")
-//     console.log("1. check Balance")
-//     console.log("2. withdraw Money")
-//     console.log("3. Deposit Balance")
-//     console.log("4. Transfer money to another user")
-//     console.log("5. view transcations")
-//     console.log("6 .change Pin")
-//     console.log("7 .Exit")
-
-
-//     const choice = prompt("Enter your choice (1-7)")
-
-//     switch (choice){
-//         case "1":
-//             // console.log("one is chosen")
-//             // checkBalance function(User);
-//             checkBalance(user)
-//             break;
-//         case "2":
-//             withdrawMoney(user)
-//             // withdraw function(user)
-//             break;
-//         case "3":
-//             // depositing balance
-//             depositMoney(user)
-//             break;
-//         case "4":
-//             // console.log("4 is chosen")
-//             // transferring money to another user
-//             transferMoney(user)
-//             break;
-//         case "5":
-//             viewTransaction(user)
-//             break;
-//         case "6":
-//             console.log("6 is chosen")
-//             break;
-//         case "7":
-//             console.log("thank you for banking with us , loggin you out")
-//             continueBanking = false;
-//             break
-//         default:
-//             console.log("please choose a valid input")
-
-//     }
-//     }
-// }
-
-
-// function checkBalance(user){
-//     console.log(`Your current Balance is : ${user.balance}`)
-// }
-
-
-// function withdrawMoney(user){
-//     // amount
-//     const amount = parseFloat(prompt("enter amout to withdraw"))
-//     // prompt converts every input to a  string
-//     // parseInt(),parseFloat()
-//     // parseInt("100") = 100
-//     // && ||
-//     // 100 - two hunder two  error  = naN
-
-//     if(isNaN(amount) ||  amount <=0){
-//         console.log("Invalid amount. please enter a positive number.")
-//         return
-//     }
-
-//     if(amount > user.balance){
-//         console.log("insufficent funds")
-//         return
-//     }
-//     // todo:this transaction must be saved
-    
-//     user.balance = user.balance - amount;
-
-//     recordTransaction(user,"Withdrawals",amount)
-
-
-//     console.log(`withdrawal successful. remaining Balance : $ ${user.balance.toFixed(2)}`)
-
-// }
-
-
-// function depositMoney(user){
-//    const amount = parseFloat(prompt("enter the amount to deposit: "))
-
-//    if(isNaN(amount) ||  amount <=0){
-//     console.log("invalid amount, please enter a positive number")
-//     return
-//    }
-
-//    user.balance = user.balance + amount;
-
-//  recordTransaction(user,"Deposits",amount)
-// //    save transaction
-// console.log(`Deposit succesful. New Balance:${user.balance.toFixed(2)}`)
-
-
-// }
-
-// function transferMoney(sender){
-//     // 1.we need data of the sender
-//     // 2.prompt the user to provide a valid user account
-//     // 3.fetch the user with the provided account number 
-//     const recipientAccount  = prompt("enter the recipients 8-digit account number:")
-//     const recipient  = users.find(
-//         (user)=>{
-//             return user.accountNumber === recipientAccount && user.accountNumber !== sender.accountNumber
-//         }
-//     )
-//     // guard clause
-//     if(!recipient){
-//             console.log("reciepient not found or cannot transfer money to yourself")
-//             return
-//     }
-//     const amount = parseFloat(prompt(`Enter amount to transfer to ${recipient.name} , Account No: ${recipient.accountNumber} :`))
-
-//     if(isNaN(amount) || amount <=0){
-//         console.log("Invalid amount. Please enter a valid amount")
-//         return 
-//     }
-//     // we perform the transaction
-//     sender.balance = sender.balance - amount;
-//     recipient.balance = recipient.balance + amount;
-//     // 1.we ask the user to enter amount to be transfered
-//     // 2.we deduct amount from sender and credit // add that amount to the recipeint
-//     // watch out for the edge cases
-
-//      recordTransaction(sender,"Transfers",amount)
-//     console.log(`Transfer successful. Your balance has been debited with - ${amount}`)
-
-// }
-
-// // 1. we need a function to save all of our transactions
-
-
-// // transactions:[{
-// //  date:, type:"deposit"||"credit",amount:500
-// // }]
-
-// function viewTransaction(user){
-//     if (user.transcations.length === 0){
-//         console.log("No transactions found");
-//         return
-//     }
-
-//     console.log("Filter transactions By: ")
-//     console.log("1. View All")
-//     console.log("2. View Withdrawals")
-//     console.log("3. View Deposits")
-//     console.log("4. View Transfers")
-//     console.log("5. Go Back to the main Menu")
-
-
-//     const filterChoice = prompt("Enter your choice(1-5)");
-
-//     let filteredTransaction = [];
-
-//     switch(filterChoice){
-//         case "1":
-//             filteredTransaction = user.transcations;
-            
-//             break;
-//         case "2":
-//             filteredTransaction = user.transcations.filter((transaction)=>{
-//               return  transaction.typeOfTransction.includes("Withdrawals")
-//             })
-//             break
-//         case "3":
-//             filteredTransaction = user.transcations.filter((transaction)=>{
-//               return   transaction.typeOfTransction.includes("Deposits")
-//             })
-//             console.log(filteredTransaction)
-//             break
-//         case "4":
-//             filteredTransaction = user.transcations.filter((transaction)=>{
-//               return   transaction.typeOfTransction.includes("Transfers")
-//             })
-//             break
-//         case "5":
-//             return
-//         default:
-//             console.log("Invalid choice")
-//             return         
-//     }
-
-//     if(filteredTransaction.length === 0){
-//         console.log("No transactions found")
-//         return
-//     }
-
-//     filteredTransaction.forEach((transaction,index)=>{
-//         console.log(`${index + 1} ${transaction.date} - ${transaction.typeOfTransction} - $ ${transaction.amount.toFixed(2)}`)
-//     })
-
-// }
-
-
-
-
-
-
-
-
-
-// function recordTransaction(user,type,amount){
-//     user.transcations.push({
-//         date:new Date().toLocaleString(),
-//         typeOfTransction:type,
-//         amount:amount
-//     })
-// }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// // // Objects in Javascript
-
-// // // objects in javascript
-
-// // const person = {
-// //     firstName:"Binyamin",
-// //     lastName:"Tamirat",
-// //     age:12,
-// //     dob:"January 2010",
-// //     schoolsAttended:["Interlakes","harvard","yale"],
-
-// //     // method
-// //     greetPeople:function(name){
-// //       console.log ( "hello people I am binyamin",name)
-// //     }
-// // }
-
-// // const firstName = "john doe"
-// // const person1 = ["binyamin","tamirat",12,"January 2010"]
-
-// // console.log(person.firstName,person.lastName)
-// // console.log(person.age)
-// // // Bank system
-
-// // console.log(person.greetPeople("abebe"))
-
-// sd cxxcxxcxcxxccxcxcxccxcxcxcxc
 
 const newDiv = document.createElement("div")
+
 const parent = document.querySelector(".parent-container")
-newDiv.textContent = "hey their am the new div"
+// newDiv.textContent = "hey there am the new div"
+newDiv.textContent = "<h1>hey there</h1>"
 parent.appendChild(newDiv)
-const newButton = documentElement("h1")
-newButton.textContent = "clock me"
-parent.appendChild(newButton)
+parent.prepend(newDiv)
+
+
+const newButton = document.createElement("button")
+const container = document.querySelector(".list")
+newButton.textContent  = "click me"
+container.appendChild(newButton)
+
+// const newParagraph  = document.createElement('p')
+// newParagraph.textContent="new paragraph"
+// parent.appendChild(newParagraph)
+// parent.prepend(newParagraph)
+
+// append
+// prepend
+// after
+
+// parent.after(newParagraph)
+
+parent.insertAdjacentHTML("beforeend","<button>New paragraph</button>")
+parent.insertAdjacentHTML("beforebegin","<h2>super parent</h2>")
+parent.insertAdjacentHTML("afterbegin","<h3>original child</h3>")
+parent.insertAdjacentHTML("afterend","<a>click here</a>")
+
+
+// const inputtext = input.textContent
+// p.textContext = inputtext
+// parent.insertAdjacentHTML("afterbegin",`<p>${p.textcontet}</p>`)
+
+//postions-description
+
+// beforebegin-before the element
+// afterbegin-inside the parent before the first child
+// beforeend-inside,after the child
+// afterend-after the element
+
+// Replacing a child
+
+const originalChild = document.querySelector(".original-child")
+const newChild = document.createElement("p")
+
+newChild.textContent = "New Child"
+parent.replaceChild(newChild,originalChild)
+
+const clone = parent.cloneNode(true);
+
+parent.after(clone)
+
+
+// removing a child
+
+parent.removeChild(newChild)
+
+
+const listItem = document.createElement("li")
+listItem.textContent = "new item"
+const firstChilde = document.querySelector(".highlight")
+container.insertBefore(listItem,firstChilde)
+
+
+
+
+// to create an html element we use createElement()
+// to add text content we either use element.textContent or element.innerHtml
+// insert it to the dom as a child we use appendChild(element)
+// const newHeader = document.createElement('h1')
+// newHeader = "at the top"
+// parent.appendChild(newHeader)
 
 
 
 
 
+{/* <div>
+    hey there am the new div
+</div> */}
 
 
 
-// // Banking system
-// // 1.withdraw money
-// // 2.transfer money
-// // 3.check our balances
-// // 4 pin change
+// javascript Dom
+
+// 1. what is the dom
+
+
+// 2. what is nodes and types of nodes
+
+
+
+// 3. Selecting elements using Dom
+    // 1.getElementById()
+    // 2.getelementsByname()
+    // 3.getelementbytagname()
+    // 4getelementby classname()
+    //5querySelector() and querySelectorAll)()
+
+
+// 4. traversing elements
+        // -selecting parent element
+        // -slecting child element
+        // selecting sibling element
+
+// 5.manipulating html elements
+
+        // createElement()
+        // appendChild()
+        // textContent
+        // innerHtml
+        // after() 
+        // append()
+        // prepend()
+        // insertAdjacentHtml()
+        // replaceChild()
+        // cloneNode()
+        // removeChild()
+        // insertBefore()
+
+// 6.Attribute methods
+
+    // 1.getAttribute()
+    // 2.setAttribute() 
+    // hasAttribute()
+    // removeAttribute()
+
+// 7.Manipulating element's Styles
+    // 1.style property
+    // 2.cssText
+    // 3.getComputedStyle
+    // 4.classList property
+
+// 8 Javascript Events
+// 1.what is event
+// 2.event bubbling & event capturing
+// 3.event handler in html attributes
+// 4.dom level 0 event handlers
+// 5. add eventListenr()
+// 6. removeEventListener()
+// 7.event object
+// 8.different types of events xcv
+
