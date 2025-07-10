e02a16c939ca14c03001f703ddc4da04ca19b1bb
dagimmengestu5 <dagimmengestu5@gmail.com>
2025-07-10 20:32:04 +0300

    df

--- Html and css/Day 7/7.js
+++ Html and css/Day 7/7.js
@@ -26,7 +26,7 @@ const allspecialParagraph = document.querySelectorAll(".special")
 
 // const parent = document.querySelector(".parent-container")
 // const allChildren = parent.children;
-// console.log(parent,allChildren)dsdssdsddswdse
+// console.log(parent,allChildren)dsdssdsddsdfwdse
 
 
 const newDiv = document.createElement("div")
