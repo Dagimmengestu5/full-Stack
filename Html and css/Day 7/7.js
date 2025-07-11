e3ecf2e707f0959c20ead5d726a1c19db0332663
dagimmengestu5 <dagimmengestu5@gmail.com>
2025-07-11 20:17:28 +0300

    qw

--- Html and css/Day 7/7.js
+++ Html and css/Day 7/7.js
@@ -26,7 +26,7 @@ const allspecialParagraph = document.querySelectorAll(".special")
 
 // const parent = document.querySelector(".parent-container")
 // const allChildren = parent.children;
-// console.log(parent,allChildren)dfdsdssddfdsddcffdsdsdfsdfdfddfdfdfsdsdfwdse
+// console.log(parent,allChildren)dfdsdssddfdadwsddcffdsdsdfsdfdfddfdfdfsdsdfwdse
 
 
 const newDiv = document.createElement("div")
