a7ac916d0c4efa0babf8e2fb0ac19c2b643fdf26
dagimmengestu5 <dagimmengestu5@gmail.com>
2025-07-18 21:05:36 +0300

    2

--- Html and css/Day 8/8.js
+++ Html and css/Day 8/8.js
@@ -6,7 +6,7 @@ const emptyStatus = document.getElementById("emptyStatus")
 const totoalTasks = document.getElementById("totalTasks")
 const completesTask = document.getElementById("complitedTask")
 
-//g
+//gf
 
 let todos = []
 function addTodo () {
