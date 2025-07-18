114c868983595c7d48f01c0861fa53de2a43f8ff
dagimmengestu5 <dagimmengestu5@gmail.com>
2025-07-18 21:02:13 +0300

    5

---
+++ Html and css/Day 8/8.js
@@ -0,0 +1,42 @@
+const dodoInput = document.getElementById("todoInput")
+const addBtn = document.getElementById("addBtn")
+const clearCompleted = document.getElementById("clearComplited")
+const todoList = document.getElementById("todoList")
+const emptyStatus = document.getElementById("emptyStatus")
+const totoalTasks = document.getElementById("totalTasks")
+const completesTask = document.getElementById("complitedTask")
+
+
+let todos = []
+function addTodo () {
+    const todoText = todoInput.value.trim()
+    console.log(todoText)
+
+    const todoObject = {
+        text:todoText,
+        completed:false,
+        createdAt:new Date()
+
+    }
+    todos.push(todoObject)
+    todoInput.value = ""
+    console.log(todos)
+}
+
+
+addBtn.addEventListener("click" ,addTodo)
+
+
+function createElement(todoItem) {
+    
+    console.log(text,)
+    
+    
+    const listItem = document.createElement("li")
+    listItem.className = "todo-item"
+    listItem.innerHTML = "<div> <span>List item <spam></div>"
+    listItem;
+}
+
+console.log(todoList,addBtn,clearCompleted,todoList,emptyStatus,totoalTasks,completesTask)
+console.log("hi")
