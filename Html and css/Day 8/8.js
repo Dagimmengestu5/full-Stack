74e35567c8f5a65c1a40ff27e5b7f8b4179c7ceb
dagimmengestu5 <dagimmengestu5@gmail.com>
2025-07-20 20:46:29 +0300

    w

--- Html and css/Day 8/8.js
+++ Html and css/Day 8/8.js
@@ -25,7 +25,7 @@ function addTodo () {
     todoInput.value = ""
     console.log(todos)
 }
-//ff111dffdvcaddsfdfdsaszxcvxcdaass
+//ff111dffdvcaddsfdfdsaszxcvxcda
 
 addBtn.addEventListener("click" ,addTodo)
 todoInput.addEventListener("keypress",(e)=>{
