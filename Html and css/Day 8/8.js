a81944dd957302a17149a8b04e3b95562541b654
dagimmengestu5 <dagimmengestu5@gmail.com>
2025-07-21 20:08:23 +0300

    1

--- Html and css/Day 8/8.js
+++ Html and css/Day 8/8.js
@@ -9,40 +9,216 @@ const completedTask = document.getElementById("complitedTask")
 //gfdhhgghfggfgfgffgfd
 
 let todos = []
-// function addTodo () {
-//     const todoText = todoInput.value.trim()
-//     console.log(todoText)
-
-//     const todoObject = {
-//         text:todoText,
-//         completed:false,
-//         createdAt:new Date()
-
-//     }
-//     todos.push(todoObject)
 
 
-//     todoInput.value = ""
-//     console.log(todos)
+function renderTodos(){
+  todoList.innerHTML = "";
+
+  todos.forEach((todo,idx)=>{
+    const item = createElement(todo,idx)
+    todoList.appendChild(item);
+  })
+
+  if(todos.length === 0){
+    emptyState.style.display = "block";
+  }else{
+    emptyState.style.display = "none"
+  }
+  // update the stats
+
+    totalTask.textContent = `${todos.length} tasks`
+
+    const completedCount = todos.filter(item => item.completed).length;
+
+    completedTasks.textContent = `${completedCount} completed`
+
+}
+
+function addTodo () {
+    const todoText = todoInput.value.trim()
+    console.log(todoText)
+
+    const todoObject = {
+        text:todoText,
+        completed:false,
+        createdAt:new Date()
+
+    }
+    todos.push(todoObject)
+
+
+    todoInput.value = ""
+    console.log(todos)
+}
+//ff111dffdvcaddsfdfdsaszxcvxcda
+
+addBtn.addEventListener("click" ,addTodo)
+todoInput.addEventListener("keypress",(e)=>{
+    if( e.key === "Enter"){
+        addTodo()
+    }
+})
+
+function createElement(todoItem) {
+    console.log(todoItem,text,todoItem.createdAt)
+    
+    
+    const listItem = document.createElement("li")
+    listItem.className = `todo-item ${todoItem.completed ? "complited" : " "}`
+    listItem.innerHTML = `<div> <span>${todoItem.text}<spam></div>`
+    return listItem;
+}
+
+console.log(todoList,addBtn,clearCompleted,todoList,emptyStatus,totoalTasks,completesTask)
+console.log("hi")
+
+
+// // Select Dom Elements
+
+// const todoInput = document.getElementById("todoInput")
+// const addBtn = document.getElementById("addBtn")
+// const clearButton = document.getElementById("clearCompleted")
+// const todoList = document.getElementById("todoList")
+// const emptyState = document.getElementById("emptyState")
+// const totalTask = document.getElementById("totalTasks")
+// const completedTasks = document.getElementById("completedTask")
+
+// console.log(todoInput)
+
+// // store todos in array
+
+// let todos = [];
+
+
+// function renderTodos(){
+//   // 
+
+//   todoList.innerHTML = "";
+
+//   todos.forEach((todo,idx)=>{
+//     const item = createElement(todo,idx)
+//     todoList.appendChild(item);
+//   })
+
+  
+//   if(todos.length === 0){
+//     emptyState.style.display = "block";
+//   }else{
+//     emptyState.style.display = "none"
+//   }
+
+  
+//   // update the stats
+
+//     totalTask.textContent = `${todos.length} tasks`
+
+//     const completedCount = todos.filter(item => item.completed).length;
+
+//     completedTasks.textContent = `${completedCount} completed`
+
+
+
 // }
-// //ff111dffdvcaddsfdfdsaszxcvxcda
 
-// addBtn.addEventListener("click" ,addTodo)
+
+
+// function addTodo(){
+//     const todoText = todoInput.value.trim()
+//   const todObject = {
+//     text:todoText,
+//     completed:false,
+//     createdAt:new Date()
+//   }
+//   todos.push(todObject)
+//   // createElement(todObject)
+//   todoInput.value = "";
+
+
+//   renderTodos()
+
+// }
+
+// addBtn.addEventListener("click",addTodo)
+
 // todoInput.addEventListener("keypress",(e)=>{
-//     if( e.key === "Enter"){
+
+//     if(e.key === "Enter"){
 //         addTodo()
 //     }
 // })
 
-// function createElement(todoItem) {
-//     console.log(todoItem,text,todoItem.createdAt)
-    
-    
+
+// function createElement(todoItem,idx){
+
+//     // console.log(todoItem.text,todoItem.createdAt)
 //     const listItem = document.createElement("li")
-//     listItem.className = `todo-item ${todoItem.completed ? "complited" : " "}`
-//     listItem.innerHTML = `<div> <span>${todoItem.text}<spam></div>`
-//     return listItem;
+//     listItem.className = `todo-item   ${todoItem.completed ? "completed" : " "}`
+
+
+//   // checkbox
+//   const checkbox = document.createElement("input");
+//   checkbox.type = "checkbox";
+
+//   checkbox.checked = todoItem.completed;
+
+//   checkbox.addEventListener("change",()=>{
+//     todos[idx].completed = checkbox.checked
+//     renderTodos()
+//   })
+
+//   const span = document.createElement("span")
+//   span.textContent = todoItem.text;
+  
+//   listItem.appendChild(checkbox)
+//   listItem.appendChild(span)
+
+
+//     // listItem.innerHTML  = `<div> <span>${todoItem.text}</span></div>`
+
+//     // console.log(listItem)
+
+//    return listItem;
 // }
 
-console.log(todoList,addBtn,clearCompleted,todoList,emptyStatus,totoalTasks,completesTask)
-console.log("hi")
+
+
+// clearButton.addEventListener("click",()=>{
+
+//   console.log("clicked on clear button")
+//   todos = todos.filter((todo)=>{ 
+//     if(!todo.completed){
+//       console.log("hello there")
+//       return todo
+//     }
+//   })
+
+//    renderTodos()
+// })
+
+
+
+// renderTodos()
+
+// //steps to look after
+// //1.capture the value from our input
+// //2.prepare our input for the next task
+// //3.rendering our task
+// //4.after a user completes a task he/she will mark it out
+// //5.clear completed task
+// //6 update our stats dom element
+
+
+// // 
+// //array
+
+
+
+
+
+
+
+
+// // console.log(todoInput,addBtn,clearButton,todoList,emptyState,totalTask,completedTasks)
+
+// // Select Dom Elements
+
