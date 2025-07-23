104b4a6f665feccad7d44b8efba12a20994150fa
dagimmengestu5 <dagimmengestu5@gmail.com>
2025-07-23 21:22:25 +0300

    2

--- Html and css/Day 8/8.js
+++ Html and css/Day 8/8.js
@@ -73,7 +73,7 @@ console.log(todoList,addBtn,clearCompleted,todoList,emptyStatus,totoalTasks,comp
 console.log("hi")
 
 
-// // Select Dom Elements s2ssssdss
+// // Select Dom Elements s2ssssdsss
 
 // const todoInput = document.getElementById("todoInput")
 // const addBtn = document.getElementById("addBtn")
