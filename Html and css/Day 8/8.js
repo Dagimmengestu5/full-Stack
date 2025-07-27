bf8caf700cee441327bc7df66065cb26cbdbf42a
dagimmengestu5 <dagimmengestu5@gmail.com>
2025-07-27 20:47:18 +0300

    d

--- Html and css/Day 8/8.js
+++ Html and css/Day 8/8.js
@@ -114,7 +114,7 @@ function createElement(todoItem,idx){
 
     // listItem.innerHTML  = `<div> <span>${todoItem.text}</span></div>`
 
-    // console.log(listItem)sfgfdsfdsddsgdddsfdfsdsddfdfdfdfdfdf
+    // console.log(listItem)sfgfdsfdsddsgddddsfdfsdsddfdfdfdfdfdf
 bv
    return listItem;
 }
